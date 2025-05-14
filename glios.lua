1e44e54e952dcaab7c47264a01587496ce8b4130
kingrainerboi <kingrainmendoza282000@gmail.com>
2025-05-14 05:51:08 -0400

    v?

--- glios.lua
+++ glios.lua
@@ -104,7 +104,7 @@ local function createDashGui()
 
 	local button = Instance.new("TextButton")  
 	button.Size = UDim2.new(0, 160, 0, 30)  
-	button.Position = UDim2.new(1, -170, 1, 50)  
+	button.Position = UDim2.new(1, -170, 1, -100)  
 	button.AnchorPoint = Vector2.new(0, 1)  
 	button.Text = "edash"  
 	button.BackgroundColor3 = Color3.fromRGB(80, 80, 80)  
