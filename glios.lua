0ad1473b38cede928a1120deae6ca4b47d57b238
kingrainerboi <kingrainmendoza282000@gmail.com>
2025-05-15 14:20:56 -0400

    v

--- glios.lua
+++ glios.lua
@@ -691,7 +691,7 @@ local function Ult()
 	if kelerEnabled then
 		duration = 7
 		radius = 5.5
-		dashSpeed = MAX_DASH_SPEED * 2.25
+		dashSpeed = MAX_DASH_SPEED * 2
 	else
 		dashSpeed = MAX_DASH_SPEED * 2
 		duration = 3.5
