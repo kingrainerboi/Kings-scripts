43b15b8c5225f43b5f41e4d779e231606c5f9c4f
kingrainerboi <kingrainmendoza282000@gmail.com>
2025-05-17 05:32:58 -0400

    v1

---
+++ SCRIPTS/GLIOS_V_HK1/Glios_v_HK1.lua
@@ -0,0 +1,5 @@
+print("Loading auto glios script...")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/Kings-scripts/refs/heads/main/SCRIPTS/GLIOS_V_HK1/scripts/auto%20glios"))()
+
+print("Loading glios.lua script...")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kingrainerboi/Kings-scripts/refs/heads/main/SCRIPTS/GLIOS_V_HK1/scripts/glios.lua"))()
